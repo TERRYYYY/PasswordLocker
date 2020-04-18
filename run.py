4d41c7b92640b23b3ce3d093d956e4a1089df6ed
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-18 16:10:29 +0300

    added function that create a new credential

--- run.py
+++ run.py
@@ -9,19 +9,42 @@ def create_user(username,password):
     '''
     new_user = User(username,password)
     return new_user
+
+#===== save the newly created account ====
 def save_user(user):
     '''
     Function to save user
     '''
     user.save_user()
 
+#===== delete an existing user ====
 def del_user(user):
     '''
     Function to delete a user
     '''
     user.delete_user()
+
+#==== verify existing user ==============
 def check_existing_users(name):
     '''
     Function that checks if a user exists with that name and return boolean
     '''
+
     return User.user_exist(name)
+
+def create_credential(account,acc_username,acc_password):
+    '''
+    Function to create a new credential
+    '''
+    new_credential = Credential(account,acc_username,acc_password)
+    return new_credential
+
+# def save_credential(credential):
+#     '''
+#     Function to save credentials
+#     '''
+#     credential.save_credential()
+
+
+
+
