eab90a76388f4a4a0e618a0d43a557e2c23da7cb
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-19 20:11:12 +0300

    improvements

--- test.py
+++ test.py
@@ -63,44 +63,44 @@ class TestCredential(unittest.TestCase):
         self.assertEqual (self.new_credential.acc_username,"terrykariuki")
         self.assertEqual (self.new_credential.acc_password,"girlsrule")
 
-    # def test_save_credential(self):
-    #     '''
-    #     test_save_credential test to test if the credential object is saved into the credential list
-    #     '''
-    #     self.new_credential.save_credential() #saving a new user
-    #     self.assertEqual(len(Credential.credential_list))
+    def test_save_credential(self):
+        '''
+        test_save_credential test to test if the credential object is saved into the credential list
+        '''
+        self.new_credential.save_credential() #saving a new user
+        self.assertEqual(len(Credential.credential_list))
 
-    # def test_delete_credential(self):
-    #     '''
-    #     test_delete_credential to test if we can remove a credential from our cr4edential list
-    #     '''
-    #     self.new_credential.save_credential()
-    #     test_credential = Credential("Instagram" "terrykariuki","hey123")
-    #     test_credential.save_credential()
+    def test_delete_credential(self):
+        '''
+        test_delete_credential to test if we can remove a credential from our cr4edential list
+        '''
+        self.new_credential.save_credential()
+        test_credential = Credential("Instagram" "terrykariuki","hey123")
+        test_credential.save_credential()
 
-    #     self.new_credential.delete_credential() #deleting a user
-    #     self.assertEqual(len(Credential.credential_list),1)
+        self.new_credential.delete_credential() #deleting a user
+        self.assertEqual(len(Credential.credential_list),1)
 
-    # def test_find_credential_by_username(self):
-    #     '''
-    #     test to check if we can find a credential by account username and display info
-    #     '''
-    #     self.new_credential.save_credential()
-    #     test_credential = Credential("Instagram" "terrykariuki","hey123")
-    #     test_credential.save_credential()
+    def test_find_credential_by_username(self):
+        '''
+        test to check if we can find a credential by account username and display info
+        '''
+        self.new_credential.save_credential()
+        test_credential = Credential("Instagram" "terrykariuki","hey123")
+        test_credential.save_credential()
 
-    #     found_username = Credential.find_by_username("Instagram")
+        found_username = Credential.find_by_username("Instagram")
 
-    #     self.assertEqual(found_username.account,test_credential.account)
+        self.assertEqual(found_username.account,test_credential.account)
 
-    # def test_display_credential(self):
-    #     '''
-    #     test whether one can view all the account credentials that they have created/saved
-    #     '''
-    #     self.new_credential.save_credential()
-    #     account_2 = Credential("Facebook","Rihanna","mypassword")
-    #     account_2.save_credential()
-    #     self.assertEqual)(len(Credential.credential_list))
+    def test_display_credential(self):
+        '''
+        test whether one can view all the account credentials that they have created/saved
+        '''
+        self.new_credential.save_credential()
+        account_2 = Credential("Facebook","Rihanna","mypassword")
+        account_2.save_credential()
+        self.assertEqual)(len(Credential.credential_list))
 
 
 if __name__ == '__main__':
